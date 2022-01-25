ef019463be4173511cdf4bb4d652b81382850c80
Gabriela Arrieta <gabriela.arrieta@fluxit.com.ar>
2022-01-25 20:59:10 -0300

    new routes

--- routes/player1.js
+++ routes/player1.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
 const router = Router();
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+var serverStatus = 'IDLE';
 
 router.get('/', async(req, res) => {
     console.log('conexion a player1')
@@ -8,24 +11,42 @@ router.get('/', async(req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    // chequear si el status del server es idle
-
-    const msg = {
-        msg: "lets play"
-    }
-
     try{
-         const resp = await axios.post('http://localhost:3002/api', msg)
-         console.log('la resp data es: ', resp.data);
-         res.send('hola')
+        const resp = await axios.post('http://localhost:3002/player2', {
+            msg: "lets play"
+        })
+
+        if(resp.data.status === "SUCESS"){
+            serverStatus = 'WAITING RULES'
+            res.status(200).send('OK')
+        } 
+        
     } catch(error){
         console.log(error)
     }
 })
 
 router.post('/rules', async(req, res) => {
+    const rulesP2 = req.body.rules
+    
+    try {
+        if(Object.keys(rulesP2).length === 3 && serverStatus === 'WAITING RULES') {
+            serverStatus = 'SETTING UP'
+            res.status(200).json({
+                status: 'SUCCESS',
+            })
+        }
+        else{
+            throw new Error('Rules incomplete')
+        }
+    } catch (error) {
+        res.status(404).send(error)
+    }
+    
+});
 
-    console.log(req.rules)
+router.post('/init', async(req, res) => {
+    
 })
 
 module.exports = router;
--- routes/player2.js
+++ routes/player2.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 const axios = require('axios');
-
+var serverStatus = 'IDLE'
 
 router.get('/test', async(req, res) => {
     //console.log('conexion a player2')
@@ -9,18 +9,40 @@ router.get('/test', async(req, res) => {
 })
 
 router.post('/', async(req, res) => {
-
-    const rules = req.body.rules;
-
     try{
-        const resp = await axios.post('http://localhost:3001/api/rules', rules)
-        res.json({
-            status: 'SUCCESS',
-            data: resp 
-        })
+        if(req.body.msg === 'lets play' && serverStatus === 'IDLE'){
+            serverStatus = 'THINKING RULES'
+            res.status(200).json({
+                status: 'SUCCESS',
+            })
+        }else{
+            throw new Error('Server not prepared or msg incorrect')
+        }
    } catch(error){
        console.log(error)
    }
+})
+
+router.post('/rules', async(req, res) => {
+    const {rules} = req.body;
+    try{
+        const resp = await axios.post('http://localhost:3001/player1/rules', {
+           rules
+        })
+
+        if(resp.data.status === 'SUCCESS'){
+            serverStatus = 'RIVAL WAITING'
+            res.status(200).send('OK')
+        }
+        else{
+            throw new Error('Failed to deliver the rules')
+        }
+    
+    } catch(error){
+        console.log(error.message)
+        res.status(404)
+    }
+    
 })
 
 module.exports = router;
