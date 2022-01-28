32d941de06fb0d771134aa1cf5a4427522454e73
Gabriela Arrieta <gabriela.arrieta@fluxit.com.ar>
2022-01-27 21:12:56 -0300

    controllers player1

--- controllers/player1.controller.js
+++ controllers/player1.controller.js
@@ -5,6 +5,8 @@ const generateGridData = require("../helpers/generateGrid");
 const gridPositions = require("../helpers/gridpositions");
 let serverStatus = "IDLE";
 let grill;
+let grid;
+
 
 const getPlayerApi = (req, res) => {
   console.log("conexion a player1");
@@ -12,66 +14,68 @@ const getPlayerApi = (req, res) => {
 };
 
 const postChallenge = async (req, res) => {
-  try {
-    const resp = await axios.post("http://localhost:3002/player2/challenge", {
-      msg: "lets play",
-    });
-    if (resp.data.status === "SUCCESS") {
-      serverStatus = "WAITING RULES";
-      res.status(200).send("OK");
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  try{
+    const resp = await axios.post('http://localhost:3002/player2/challenge', {
+        msg: "lets play"
+    })
+    if(resp.data.status === "SUCCESS"){
+        serverStatus = 'WAITING RULES'
+        res.status(200).send('OK')
+    } 
+    
+} catch(error){
+    console.log(error)
+}
 };
 
 const postRules = async (req, res) => {
-  let rulesP2 = req.body.rules;
-  serverStatus = "WAITING RULES";
-  console.log("reglas p2", rulesP2);
-  console.log("status P1", serverStatus);
-  try {
-    if (Object.keys(rulesP2).length === 3 && serverStatus === "WAITING RULES") {
-      serverStatus = "SETTING UP";
-      //TODO: subir grilla
-      grill = generateGridData(rulesP2.width, rulesP2.heigth);
-      console.log(grill1);
-      res.status(200).json({
-        status: "SUCCESS",
-      });
-    } else {
-      throw new Error("Rules incomplete");
+  let rulesP2 = req.body.rules
+    serverStatus = 'WAITING RULES'
+    console.log('reglas p2', rulesP2)
+    //console.log('status P1', serverStatus)
+    try {
+        if(Object.keys(rulesP2).length === 3 && serverStatus === 'WAITING RULES') {
+            serverStatus = 'SETTING UP'
+
+            grid = generateGridData(rulesP2.width, rulesP2.heigth);
+            console.log('grilla p1', grid)
+            res.status(200).json({
+                status: 'SUCCESS',
+            })
+        }
+        else{
+            throw new Error('Rules incomplete')
+        }
+    } catch (error) {
+        console.log('error /rules p1', error.message)
+        res.status(404).send(error)
     }
-  } catch (error) {
-    console.log("error /rules p1", error.message);
-    res.status(404).send(error);
-  }
 };
 
 const postInit = async (req, res) => {
   try {
     const { positions } = req.body;
-    const reqPath = path.join(__dirname, "../uploads/positionP1.txt");
+    const reqPath = path.join(__dirname, '../uploads/positionP1.txt');
 
-    serverStatus = "RIVAL WAITING"; //eliminar
-
-    if (serverStatus === "RIVAL WAITING" || serverStatus === "SETTING UP") {
-      serverStatus = "PROCESSING PLACEMENT";
-      fs.writeFileSync(reqPath, JSON.stringify(positions));
-      //TODO: subir a la grilla las posiciones por cada uno de los elementos del array
-      for (let pos in positions) {
-        gridPositions(grill, pos);
-      }
-      console.log(grill);
-      serverStatus = "WAITING RIVAL";
-      res.status(200).send("OK");
-    } else {
-      throw new Error("Server is not on the mood");
+     serverStatus = 'RIVAL WAITING'; //eliminar
+    
+    if(serverStatus === 'RIVAL WAITING' || serverStatus === 'SETTING UP'){
+        serverStatus = 'PROCESSING PLACEMENT'
+        fs.writeFileSync(reqPath, JSON.stringify(positions))
+        //TODO: subir a la grilla las posiciones por cada uno de los elementos del array
+        for(let pos in positions){
+            gridPositions(grid, positions[pos]);
+        } 
+        console.log('grilla + posiciones P1', grid)
+        serverStatus = 'WAITING RIVAL'
+        res.status(200).send('OK')
+    } else{
+        throw new Error('Server is not on the mood')
     }
-  } catch (error) {
-    console.log("error /init p1", error.message);
-    res.status(404).send(error);
-  }
+} catch (error) {
+    console.log('error /init p1', error.message)
+    res.status(404).send(error)
+}
 };
 
 // //TODO: este ready es necesario?
@@ -92,34 +96,37 @@ const postInit = async (req, res) => {
 //
 
 const postShot = async (req, res) => {
-  const { X, Y } = req.params;
-  const positionShot = X + Y;
+  const { X, Y} = req.params;
+  const positionShot = X+Y;
+
 
   try {
-    //
-    // const reqPath = path.join(__dirname, '../uploads/positionP2.txt');
-    // const data = fs.readFileSync(reqPath, 'utf8');
-    // console.log(JSON.parse(data));
+      //
+      // const reqPath = path.join(__dirname, '../uploads/positionP2.txt');
+      // const data = fs.readFileSync(reqPath, 'utf8');
+      // console.log(JSON.parse(data));
 
-    //TODO: enviar las coordenadas positionShot al Rival
-    await axios.post("localhost:3002/player2/shot/:X/:Y", {
-      shot: positionShot,
-    });
+      //TODO: enviar las coordenadas positionShot al Rival
+      await axios.post("http://localhost:3002/player2/shot/:X/:Y", {
+          shot: positionShot
+      })
 
-    res.status(200).send("Send shot");
-  } catch (error) {
-    console.log(error);
+      res.status(200).send('Send shot');
+
+  } catch(error){
+      console.log(error);
   }
 };
 
-const yieldServerToIdle = async (req, res) => {
+const postYield = async (req, res) => {
   serverStatus = "IDLE";
 };
 
 module.exports = {
   getPlayerApi,
+  postChallenge,
   postRules,
   postInit,
   postShot,
-  yieldServerToIdle,
+  postYield,
 };
--- routes/player1.js
+++ routes/player1.js
@@ -1,132 +1,31 @@
 const { Router } = require("express");
 const router = Router();
-const axios = require('axios');
-const fs = require('fs');
-const path = require('path');
-const generateGridData = require("../helpers/generateGrid");
-const gridPositions = require('../helpers/gridpositions');
-let serverStatus = 'IDLE';
-let grid;
+
+const { 
+    getPlayerApi,
+    postChallenge,
+    postRules,
+    postInit,
+    postShot,
+    postYield
+ } = require('../controllers/player1.controller')
 
 
-router.get('/', async(req, res) => {
-    console.log('conexion a player1')
-    res.send('hello')
-})
+router.get('/', getPlayerApi);
 
-router.post('/challenge', async (req, res) => {
-    try{
-        const resp = await axios.post('http://localhost:3002/player2/challenge', {
-            msg: "lets play"
-        })
-        if(resp.data.status === "SUCCESS"){
-            serverStatus = 'WAITING RULES'
-            res.status(200).send('OK')
-        } 
-        
-    } catch(error){
-        console.log(error)
-    }
-})
+//TODO: tira error cuando se hace un send por segunda vez, la primera no...
+router.post('/challenge', postChallenge);
 
-router.post('/rules', async(req, res) => {
-    let rulesP2 = req.body.rules
-    serverStatus = 'WAITING RULES'
-    console.log('reglas p2', rulesP2)
-    //console.log('status P1', serverStatus)
-    try {
-        if(Object.keys(rulesP2).length === 3 && serverStatus === 'WAITING RULES') {
-            serverStatus = 'SETTING UP'
+router.post('/rules', postRules);
 
-            grid = generateGridData(rulesP2.width, rulesP2.heigth);
-            console.log('grilla p1', grid)
-            res.status(200).json({
-                status: 'SUCCESS',
-            })
-        }
-        else{
-            throw new Error('Rules incomplete')
-        }
-    } catch (error) {
-        console.log('error /rules p1', error.message)
-        res.status(404).send(error)
-    }
-    
-});
+router.post('/init', postInit);
 
-router.post('/init', async(req, res) => {
+// TODO: este ready es necesario?
+// router.post('/ready', ...)
 
-    try {
-        const { positions } = req.body;
-        const reqPath = path.join(__dirname, '../uploads/positionP1.txt');
+router.post('/shot/:X/:Y', postShot)
 
-         serverStatus = 'RIVAL WAITING'; //eliminar
-        
-        if(serverStatus === 'RIVAL WAITING' || serverStatus === 'SETTING UP'){
-            serverStatus = 'PROCESSING PLACEMENT'
-            fs.writeFileSync(reqPath, JSON.stringify(positions))
-            //TODO: subir a la grilla las posiciones por cada uno de los elementos del array
-            for(let pos in positions){
-                gridPositions(grid, positions[pos]);
-            } 
-            console.log('grilla + posiciones P1', grid)
-            serverStatus = 'WAITING RIVAL'
-            res.status(200).send('OK')
-        } else{
-            throw new Error('Server is not on the mood')
-        }
-    } catch (error) {
-        console.log('error /init p1', error.message)
-        res.status(404).send(error)
-    }
-
-});
-
-// //TODO: este ready es necesario?
-// router.post('/ready', async(req, res) => {
-
-//     try {
-                
-//         const resp = await axios.post('http://localhost:3002/player2/ready', {message: "ready"})
-//         if(resp.serverStatus=== 'SENDING SHOT') {
-//             serverStatus = 'WAITING SHOT'
-//         }
-        
-//         res.status(200).send('OK')
-//     } catch (error) {
-//         console.log('error /ready p1', error.message)
-//     }    
-    
-// })
-
-router.post('/shot/:X/:Y', async(req, res) => {
-
-    const { X, Y} = req.params;
-    const positionShot = X+Y;
-
-
-    try {
-        //
-        // const reqPath = path.join(__dirname, '../uploads/positionP2.txt');
-        // const data = fs.readFileSync(reqPath, 'utf8');
-        // console.log(JSON.parse(data));
-
-        //TODO: enviar las coordenadas positionShot al Rival
-        await axios.post("http://localhost:3002/player2/shot/:X/:Y", {
-            shot: positionShot
-        })
-
-        res.status(200).send('Send shot');
-
-    } catch(error){
-        console.log(error);
-    }
-})
-
-
-router.post('/yield', async(req, res) => {
-    serverStatus = 'IDLE';
-})
+router.post('/yield', postYield);
 
 
 module.exports = router;
