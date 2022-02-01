310f842e335cfdc7135f72b2bef2d38fa63e51bd
Gabriela Arrieta <gabriela.arrieta@fluxit.com.ar>
2022-02-01 15:35:17 -0300

    modify function

--- helpers/shotPoisition.js
+++ helpers/shotPoisition.js
@@ -1,11 +1,19 @@
-function shotPositions(positionsGrid, shot){
+function shotPositions(positionsGrid, shot, rules = {
+  patrol_boat: 2,
+  submarine: 3,
+  destroyer: 4,
+  carrier: 5,
+  }){
+  // recibir reglas de cantidad de espacios que ocupa cada barco
+  // cambiar en la grilla por una X si encuentra el barco + guardar grilla
+  // tener un contador
+  // retornar mensajes correspondientes: acerto, no acerto, acerto y se hundio el barco
   
   shot = shot.split(); // ["A5"]
 
+  // Contador
     let ships = {
-        destroyer: 4,
-        //destroyer1 ++
-        // patrol_boat2 ++  // si llega a 2 es que lo derribo
+        destroyer1: 4,  // le acerto 4 veces a ese mismo barco
       }
     
     let letra = shot[0][0];
@@ -13,13 +21,15 @@ function shotPositions(positionsGrid, shot){
 
     let barco = positionsGrid[letra][+numero]; // ej "destroyer1"
 
-    if(barco){
-      // reemplazamos ese barco por una X ?  
-
+    if(barco !== 0 && barco !== 'X'){
+      positionsGrid[letra][+numero] = 'X';
+      
       !ships[barco] ? ships[barco] = 1 : ships[barco]++
-      console.log(ships, ships[barco])
+      console.log(ships)
 
       console.log('acertaste a tu adversario')
+      console.log(positionsGrid);
+
     } else {
       console.log('sigue intentando')
     }
